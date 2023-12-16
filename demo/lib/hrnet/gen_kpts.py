f1edfeb389c1a33ad748c07ac351154276a664cf
piz2a <43025513+piz2a@users.noreply.github.com>
2023-12-17 01:35:10 +0900

    Checking 2D pose data shape

--- demo/lib/hrnet/gen_kpts.py
+++ demo/lib/hrnet/gen_kpts.py
@@ -163,4 +163,6 @@ def gen_video_kpts(video, det_dim=416, num_peroson=1, gen_output=False):
     keypoints = keypoints.transpose(1, 0, 2, 3)  # (T, M, N, 2) --> (M, T, N, 2)
     scores = scores.transpose(1, 0, 2)  # (T, M, N) --> (M, T, N)
 
+    print('keypoints:', keypoints.shape)
+    print('scores:', scores.shape)
     return keypoints, scores
